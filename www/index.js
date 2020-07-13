2d6661340a7ea07ac91d26d147cde15a72950289
Yatharth Vyas <yatharth2000.vyas@gmail.com>
2020-07-13 19:33:16 +0530

    Fixed a bug in graying of the pixels (#5)
    * Improved drawing code
    
    * Added centering of image pixels
    
    * Fixed bug
    
    * removed console log statements
    
    * removed console log statements
    
    * Fixed rounding off bug

--- www/index.js
+++ www/index.js
@@ -5,6 +5,71 @@ import * as wb from "./network.json";
 wasm.load_from_string([784, 30, 10], JSON.stringify(wb.default));
 
 document.getElementById("submit").addEventListener("click", () => {
+  var left_start = 0;
+  var right_end = 0;
+  var top_start = 0;
+  var bottom_end = 0;
+  var flag1 = false;
+  var flag2 = false;
+  for (var p = 0; p < 28; p++){
+  	for (var q = 0 ; q <28 ; q++) {
+  		if(!flag1 && digit[q][p]>0){
+  			left_start = p;
+  			flag1 = true;
+  		}
+  		if(!flag2 && digit[p][q]>0){
+  			top_start = p;
+  			flag2 = true;
+  		}
+  	}
+  	if(flag1 && flag2)
+  		break;
+  }
+  flag1 = false;
+  flag2 = false;
+  for (var p = 27; p >= 0 ; p--){
+  	for (var q = 27 ; q >= 0 ; q--) {
+  		if(!flag1 && digit[q][p]>0){
+  			right_end = p;
+  			flag1 = true;
+  		}
+  		if(!flag2 && digit[p][q]>0){
+  			bottom_end = p;
+  			flag2 = true;
+  		}
+  	}
+  	if(flag1 && flag2)
+  		break;
+  }
+  var horizontal_shift = Math.floor(14-((left_start+right_end)/2));
+  var vertical_shift = Math.floor(14-((top_start+bottom_end)/2));
+  for (var p = top_start; p <= bottom_end; p++){
+  	if((27-right_end)>left_start) // bigger gap on right side
+	  	for (var q = right_end ; q >= left_start ; q--) {
+	  		digit[p][q+horizontal_shift]=digit[p][q];
+	  		digit[p][q]=0;	
+		}
+	else
+		for (var q = left_start ; q <= right_end ; q++) {
+	  		digit[p][q + horizontal_shift]=digit[p][q];
+	  		digit[p][q]=0;	
+		}
+  }
+
+
+  for (var p = left_start + horizontal_shift; p <=  right_end +horizontal_shift; p++){
+	if(27-bottom_end > top_start) // bigger gap on bottom
+	  	for (var q = bottom_end ; q >= top_start ; q--) {
+	  		digit[q + vertical_shift][p]=digit[q][p];
+	  		digit[q][p]=0;	
+		}
+	else
+		for (var q = top_start ; q <= bottom_end ; q++) {
+	  		digit[q + vertical_shift][p]=digit[q][p];
+	  		digit[q][p]=0;	
+		}
+ }
+
   var newArr = [];
   for (var i = 0; i < 28; i++) {
     newArr = newArr.concat(digit[i]);
